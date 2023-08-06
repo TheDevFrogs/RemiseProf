00ed0c8e58fdec812fd6c7722a269ff79ba441e9
TIBILL <samuel.bilodeau@usherbrooke.ca>
2023-08-06 14:59:10 -0400

    Final
    with manual backend start

--- startAll.sh
+++ startAll.sh
@@ -2,23 +2,25 @@ echo "Demarage de keycloak..."
 cd Auth
 docker compose down
 docker compose up -d
-sleep 5
+sleep 2
 
 echo "Demarage de la DataBase..."
 cd ../DataBase
 docker compose down
 docker compose up -d
-sleep 5
+sleep 2
 
-echo "Demarage du Backend java"
-cd ../BackEnd/build/service-1.0.0-native-image-source-jar
-java -jar service-1.0.0-runner.jar &
-sleep 5
+#Procédure ne fonctionne pas pour le backend
+#echo "Demarage du Backend java"
+#cd ../BackEnd/build/service-1.0.0-native-image-source-jar
+#java -jar service-1.0.0-runner.jar &
+#sleep 5
 
 echo "Demarage du FrontEnd"
-cd ../../../FrontEnd/application
+cd ../FrontEnd/application
 npm install
+sleep 2
 ng serve --open
-sleep 5
+sleep 2
 
 
